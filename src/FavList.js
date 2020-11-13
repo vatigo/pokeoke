b15493aa6364f117810cdfeb60654b19926f96a5
Vatigo <igor.vatavuk@gmail.com>
2020-11-13 22:37:39 +0100

    Show message if there are no favorites yet

--- src/FavList.js
+++ src/FavList.js
@@ -27,13 +27,13 @@ const FavList = () => {
             <button className="favorites-open-button" onClick={toggleFavbar}>❤</button>
             <div className={"favorites-list-container " + (favbarOpen?"open":"")}>
                 <div className="favorites-title"><button onClick={toggleFavbar} className="close-favorites-button">X</button><h2>Favorites</h2></div>
-                {favorites.map(favorite => (
+                {favorites.length>0?(favorites.map(favorite => (
                     <div className="favorite-pokemon-item"  key={"fav"+favorite.id}>
                         <button className="remove-button" onClick={()=>{removeFavorite(favorite.id)}}>−</button>
                         <img className="favorite-icon" src={favorite.sprites.front_default} alt={favorite.name}></img>
                         <span>{favorite.name}</span>
                     </div>
-                ))}
+                ))):(<div className="no-favorites">No favorites here yet</div>)}
             </div>
         </div>
     );
